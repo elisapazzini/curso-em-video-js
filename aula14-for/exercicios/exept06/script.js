42533881a72ae439337849811b945e6308eec59e
elisapazzini <elisapazzini@gmail.com>
2022-10-29 18:43:48 -0300

    Adicionando exercicios 5 e 6

--- aula14-for/exercicios/exept06/script.js
+++ aula14-for/exercicios/exept06/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function gerar(){
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    
+
+    if (num.value.length == 0){
+        window.alert('[ERRO] Digite um número entre 0 e 10')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for(c = 0; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
